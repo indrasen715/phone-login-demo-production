b81071f0acf99932996a9ee74157cd296aed0b29
INDRASEN <indrasenkumar715@gmail.com>
2021-02-23 10:23:21 +0530

    Update credentials.js

--- src/config/credentials.js
+++ src/config/credentials.js
@@ -1,8 +1,8 @@
 
 constantHelper=require('./constant-helper')
 constantHelper.exportsObject = this;
-define( 'ACCOUNT_SID', "AC52ea2f9399da8696848758fe243818c7" );
-define( 'AUTH_TOKEN', "5b4ee1aa5208151a58e1f5d35abb14fe" ); 
-define( 'SERVICE_ID', "VAdd98f61d168873ad795519858d147918" ); 
-define( 'JWT_KEY', "73ad795519858d147918" );
-define('MONGO_DB_URL_STRING','mongodb+srv://test:hOJ8hnZe58Qi3nSb@cluster0.h9lga.mongodb.net/users?retryWrites=true&w=majority')
+define( 'ACCOUNT_SID', "" );
+define( 'AUTH_TOKEN', "" ); 
+define( 'SERVICE_ID', "" ); 
+define( 'JWT_KEY', "" );
+define('MONGO_DB_URL_STRING','')
